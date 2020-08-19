41d50ecff0846ff68b59fbc699fb5752a062ebc2
Brian_Izaki <bkizaki@outlook.com>
2020-08-18 23:43:39 -0300

    add anotações

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,64 +1,70 @@
-const staticCacheTeste = 'teste-3'
+const staticCacheTeste = "teste-1";
 
 // Ciclo de vida: INSTALAÇÃO (momento para "baixar" arquivos na primeira vez do SW)
-self.addEventListener('install', (event) =>{
+self.addEventListener("install", (event) => {
   // aqui é o momento de armazenar em cache os arquivos
-  
-  const urlsToCache = [
-    '/',
-    'img/lontra.jpg',
-    'style.css'
-  ];
+
+  const urlsToCache = ["/", "img/lontra.jpg", "style.css"];
 
   // sinaliza o progresso da instalação
   event.waitUntil(
     // será passado uma promisse (resolvido: salva o SW; rejeitado: descarta o SW)
-    caches.open(staticCacheTeste)
-      .then((cache) => {
-        return cache.addAll(urlsToCache)
-      })
-  )
-})
-
-
-// Ciclo de vida: ATIVAÇÂO (ocorre quando o SW se torna ativo 
-//    e o SW anterior está ausente)
-self.addEventListener('activate', (event) => {
-  // momento bom para retirar os caches antigos
-  
-  // tem significado de "espere até"
-  event.waitUntil( 
-    // caches.delete('teste-1') // forma fácil porem não é escalável
-    
-    // busca todos os caches e retorna em promisse
-    caches.keys()
-      .then((cacheNames) => {
-        // o resultado da promisse é um array com todos os nomes de cache no navegador
-        console.log(cacheNames)
-      })
+    caches.open(staticCacheTeste).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
-
 });
 
+// Ciclo de vida: ATIVAÇÂO (ocorre quando o SW se torna ativo
+//    e o SW anterior está ausente)
+self.addEventListener("activate", (event) => {
+  // momento bom para retirar os caches antigos
+
+  // tem significado de "espere até"
+  event.waitUntil(
+    // caches.delete('teste-1') // forma fácil porem não é escalável
+
+    // ✅ busca todos os caches e retorna em promisse
+    //      o resultado da promisse é um array com todos os nomes de cache no navegador
+    // ✅ filter separa os caches com nome iniciando "teste-"
+    //      e que seja diferente do cache atual
+    // ✅ o map percorre cada o array criado no filter
+    //      assim deletando todos os nomes de caches "obsoletos"
+    //      e retornando uma promisse
+    // ✅ "Promise.all" vai esperar todas as promisses que retornam do "map"
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => {
+            return (
+              cacheName.startsWith("teste-") && cacheName !== staticCacheTeste
+            );
+          })
+          .map((cacheName) => {
+            return caches.delete(cacheName);
+          })
+      );
+    })
+  );
+});
 
 // ciclo de vida: BUSCA (busca de arquivos novos a cada atualização da página)
-self.addEventListener('fetch', (event) => {
-  
+self.addEventListener("fetch", (event) => {
   // busca por requisições no cache
   event.respondWith(
-    caches.match(event.request)
+    caches
+      .match(event.request)
       .then((response) => {
-        if (response) 
-          return response;
+        if (response) return response;
         return fetch(event.request);
       })
       .catch((error) => {
-        console.log('erro: ', error)
+        console.log("erro: ", error);
       })
-  )
-  
+  );
+
   /*
-  // diz ao navegador que a gente que vai cuidar da requisição 
+  // ✅ diz ao navegador que a gente que vai cuidar da requisição 
   event.respondWith(
     // o primeiro parametro é o corpo da resposta e o segundo os cabeçalhos
     // retorna uma pagina html com "teste response"
@@ -68,10 +74,10 @@ self.addEventListener('fetch', (event) => {
       }
     }) 
   )
-  */  
+  */
 
   /*
-  // recupera uma imagem e personaliza a resposta 404
+  // ✅ recupera uma imagem e personaliza a resposta 404
   event.respondWith(
     fetch('index.html')
       .then((res) => {
@@ -86,4 +92,4 @@ self.addEventListener('fetch', (event) => {
       })
   )
   */
-})
+});
